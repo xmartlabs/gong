1b6b1a277e5cd39ba5500ed9632f65bcfa8699bd
Matías Irland <mirland@xmartlabs.com>
2020-05-14 20:38:10 -0300

    Add Router to manage app navigation

--- app/src/main/java/com/xmartlabs/template/ui/common/BaseFragment.kt
+++ app/src/main/java/com/xmartlabs/template/ui/common/BaseFragment.kt
@@ -1,31 +1,10 @@
 package com.xmartlabs.template.ui.common
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
 
 /**
  * Created by mirland on 25/04/20.
  */
-abstract class BaseFragment<V : ViewBinding> : Fragment() {
-  private var _binding: V? = null
-  protected val viewBinding get() = _binding!!
-
-  protected abstract fun inflateViewBinding(): V
-
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    super.onCreateView(inflater, container, savedInstanceState)
-    _binding = inflateViewBinding()
-    return viewBinding.root
-  }
-
-  override fun onDestroyView() {
-    _binding = null
-    super.onDestroyView()
-  }
-
-  protected inline fun withViewBinding(block: V.() -> Unit) = with(viewBinding, block)
+abstract class BaseFragment : Fragment() {
+  protected val router: NavRouter by lazy { FragmentNavRouter(this) }
 }
