95d2854be2f9dc34a73660399db796cfdd7ac049
Matías Irland <mirland@xmartlabs.com>
2020-05-19 11:49:07 -0300

    Remove router from activity

--- app/src/main/java/com/xmartlabs/template/ui/MainActivity.kt
+++ app/src/main/java/com/xmartlabs/template/ui/MainActivity.kt
@@ -3,16 +3,10 @@ package com.xmartlabs.template.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.xmartlabs.template.R
-import com.xmartlabs.template.ui.common.ActivityNavRouter
-import com.xmartlabs.template.ui.common.NavRouter
 
-class MainActivity(
-    private val router: ActivityNavRouter = ActivityNavRouter()
-) : AppCompatActivity(), NavRouter by router {
-
+class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    router.setupNavController(this, R.id.nav_host_fragment_container)
   }
 }
