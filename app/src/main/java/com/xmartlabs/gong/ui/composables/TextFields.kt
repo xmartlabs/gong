772bc1ef32e64e5a1d56222bc3a7a28c2db56c4d
sante <agustinsantellan@gmail.com>
2021-03-29 09:54:53 -0300

    Added password visibility toggle

--- app/src/main/java/com/xmartlabs/gong/ui/composables/TextFields.kt
+++ app/src/main/java/com/xmartlabs/gong/ui/composables/TextFields.kt
@@ -2,14 +2,23 @@ package com.xmartlabs.gong.ui.composables
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconToggleButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults.textFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import com.xmartlabs.gong.R
 import com.xmartlabs.gong.ui.GongColors
 import com.xmartlabs.gong.ui.Shapes
 
@@ -43,6 +52,7 @@ fun RoundedCornersPasswordTextField(
     label: @Composable () -> Unit = {},
     onValueChange: (String) -> Unit = {},
 ) {
+  var passwordShown by remember { mutableStateOf(false) }
   TextField(
       value = value,
       label = label,
@@ -53,8 +63,17 @@ fun RoundedCornersPasswordTextField(
           focusedIndicatorColor = GongColors.transparent,
           unfocusedIndicatorColor = GongColors.transparent,
       ),
-      visualTransformation = PasswordVisualTransformation(),
+      visualTransformation = if (!passwordShown) PasswordVisualTransformation() else VisualTransformation.None,
       keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+      trailingIcon = {
+        IconToggleButton(checked = passwordShown, onCheckedChange = { passwordShown = !passwordShown }) {
+          Icon(
+              painter =
+              painterResource(id = if (!passwordShown) R.drawable.ic_show_password else R.drawable.ic_hide_password),
+              contentDescription = null,
+          )
+        }
+      },
       modifier = modifier.fillMaxWidth(),
   )
 }
