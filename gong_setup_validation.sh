dc01bed4c22a6cff94054907b56a519e9456a88e
Matías Irland <mirland@xmartlabs.com>
2021-07-06 16:41:59 -0300

    Check package structure and compile project

--- gong_setup_validation.sh
+++ gong_setup_validation.sh
@@ -6,18 +6,32 @@ APP_PACKAGE="io.foo"
 
 PROJECT_FOLDER="$(pwd)"
 
+echo "Initializing project"
 rm -rf "$TEST_VERIFICATION_FOLDER"
 mkdir -p "$TEST_VERIFICATION_FOLDER"
 cd "$TEST_VERIFICATION_FOLDER" || exit
-echo -e "$APP_NAME\n$APP_PACKAGE" |"$PROJECT_FOLDER"/gong_setup.sh $TEST_MODE_ARG "$PROJECT_FOLDER"
-
+echo -e "$APP_NAME\n$APP_PACKAGE" |"$PROJECT_FOLDER"/gong_setup.sh $TEST_MODE_ARG "$PROJECT_FOLDER" > /dev/null
 cd "$TEST_VERIFICATION_FOLDER/test-foo" || exit 1
-[ -d "./build" ] && ./gradlew clean
-GONG_LINES=$(grep -irnw --exclude={README.md,gong_setup_validation.sh,versions.gradle,*.iml} --exclude-dir={.git,README.md,.gradle,.idea,secrets} $TEST_VERIFICATION_FOLDER -e 'gong')
+echo -e "  OK\n"
 
+echo "Check app's packages"
+[ -d "./build" ] && ./gradlew clean -q
+GONG_LINES=$(grep -irnw --exclude={README.md,gong_setup_validation.sh,versions.gradle,*.iml} --exclude-dir={.git,README.md,.gradle,.idea,secrets} $TEST_VERIFICATION_FOLDER -e 'gong')
 if [ -n "${GONG_LINES}" ]; then
   echo -e "Gong word appear in multiple places\n'$GONG_LINES'"
   exit 1
 fi
+echo -e "  OK\n"
 
-cd "$PROJECT_FOLDER" || exit 1
+echo "Check app's folder structure"
+cd "app/src/" || exit 1
+for folder in */ ; do
+  directory="$(pwd)/${folder}java/io/foo/"
+  [ ! -d "$directory" ] && echo "Error: Directory $directory DOES NOT exists." && exit 1
+done
+cd "../.."
+echo -e "  OK\n"
+
+echo "Compiling project"
+./gradlew build -q
+echo -e "  OK\n"
