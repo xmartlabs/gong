e4e36869c8fe77be45388d6dec5605f4810c9ac5
Matías Irland <mirland@xmartlabs.com>
2021-08-09 15:48:54 -0300

    Migrate datastore to stable version (#199)

--- app/src/main/java/com/xmartlabs/gong/data/repository/store/datastorage/JsonDataStoreEntitySerializer.kt
+++ app/src/main/java/com/xmartlabs/gong/data/repository/store/datastorage/JsonDataStoreEntitySerializer.kt
@@ -2,6 +2,9 @@ package com.xmartlabs.gong.data.repository.store.datastorage
 
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
@@ -14,19 +17,23 @@ import java.io.OutputStream
 class JsonDataStoreEntitySerializer<T>(
     private val serializer: KSerializer<T>,
     private val defaultValueCreator: () -> T,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : Serializer<T> {
     override val defaultValue: T
         get() = defaultValueCreator()
 
-    override fun readFrom(input: InputStream): T = try {
-        Json.decodeFromString(
-            serializer,
-            input.readBytes().decodeToString()
-        )
-    } catch (serializationException: SerializationException) {
-        throw CorruptionException("Unable to deserialize", serializationException)
+    override suspend fun readFrom(input: InputStream): T = withContext(dispatcher) {
+        try {
+            Json.decodeFromString(
+                serializer,
+                input.readBytes().decodeToString()
+            )
+        } catch (serializationException: SerializationException) {
+            throw CorruptionException("Unable to deserialize", serializationException)
+        }
     }
 
-    override fun writeTo(t: T, output: OutputStream) =
+    override suspend fun writeTo(t: T, output: OutputStream) = withContext(dispatcher) {
         output.write(Json.encodeToString(serializer, t).encodeToByteArray())
+    }
 }
