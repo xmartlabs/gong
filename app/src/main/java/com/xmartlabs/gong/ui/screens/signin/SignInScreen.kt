3bba2eb5a12cae81a43d68e7e2ac54efd966591e
Gustavo Franco <gfran.93@gmail.com>
2023-06-05 20:09:18 -0300

    fix: linters

--- app/src/main/java/com/xmartlabs/gong/ui/screens/signin/SignInScreen.kt
+++ app/src/main/java/com/xmartlabs/gong/ui/screens/signin/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.xmartlabs.gong.ui.screens.signin
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
@@ -109,6 +110,7 @@ fun SignInDarkPreview() {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalComposeUiApi::class)
 @Suppress("LongMethod")
 @Composable
